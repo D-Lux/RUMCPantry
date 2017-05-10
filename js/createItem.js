46c2df431e9341bc5d9530e53be40fb0d2724222
Colton <colton.carter1130@gmail.com>
2017-05-10 15:37:02 -0500

    ap_io2 (item submission)
    Most of these updates were just for ap_io2 adding items to the database.
    Feel free to try and break and give feedback.
    
    Tooltip.css is a thing we can use on text as long as the tag has a
    tooltip tag in it. Check out the example in ap_io2 for use case. You can
    hover over Factor
    
    createItem.js has the function that runs to validate the form before
    sending it off to the database.
    
    createItem.php has the actual database access and sending data.
    
    I also like to take notes as I work, more for myself but sometimes they
    help and have created a notes folder.

---
+++ js/createItem.js
@@ -0,0 +1,49 @@
+function getElementAndColorIt(elementID, color) {
+    var element = document.getElementById(elementID);
+    element.style.color = color;
+}
+
+function validateItemAdd() {
+
+    var response = "";
+    var category = document.forms["addItem"]["category"].value;
+    var itemName = document.forms["addItem"]["itemName"].value;
+    var displayName = document.forms["addItem"]["displayName"].value;
+    var price = document.forms["addItem"]["price"].value;
+    var errors = 0;
+
+
+    if (category == "" || category.length == 0 || category == null) {
+        getElementAndColorIt("category", "red");
+        errors++;
+        response += "Category field is empty. \n"
+    }
+    if (itemName == "" || itemName.length == 0 || itemName == null) {
+        getElementAndColorIt("itemName", "red");
+        errors++;
+        response += "Item name field is empty. \n"
+    }
+    if (displayName == "" || displayName.length == 0 || displayName == null) {
+        getElementAndColorIt("displayName", "red");
+        errors++;
+        response += "Display name field is empty. \n"
+    }
+    if (price == "" || price.length == 0 || price == null) {
+        getElementAndColorIt("price", "red");
+        errors++;
+        response += "price field is empty. \n"
+    }
+    if (price < 0 || price > 10000) {
+        getElementAndColorIt("price", "red");
+        errors++;
+        response += "price field contains a number that is less than 0 or greater than 10000. \n"
+    }
+
+
+    if (errors > 0) {
+        alert("There are " + errors + " errors in the form. \nPlease fix and resubmit. \nThe errors are: \n" + response);
+        return false;
+    } else {
+        return true;
+    }
+}
---
+++ php/createItem.php
@@ -0,0 +1,75 @@
+<?php
+
+function echoDivWithColorSuccess($message)
+{
+    echo  '<div style="color: green;">'; /*must do color like this, can't do these three lines on the same line*/
+    echo $message;
+    echo  '</div>';
+}
+
+function echoDivWithColorFail($message)
+{
+    echo  '<div style="color: red;">'; /*must do color like this, can't do these three lines on the same line*/
+    echo $message;
+    echo  '</div>';
+}
+
+if(isset($_POST['submit'])) /*when the button is pressed on post request*/
+{
+    
+
+    $category = $_POST['category'];
+    $itemName = $_POST['itemName']; /*grab the name textbox*/
+    $displayName = $_POST['displayName'];
+    $price = $_POST['price'];
+
+    $small = $_POST['small'];
+    $medium = $_POST['medium'];
+    $large = $_POST['large'];
+    $walkIn = $_POST['walkIn'];
+
+    $factor = $_POST['factor'];
+
+    $servername = "127.0.0.1";
+    $username = "root";
+    $password = "";
+    $dbname = "foodpantry";
+    /* previous lines set up the strings for connextion*/
+
+    /* Create connection*/
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    /* Check connection*/
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } 
+
+    $sql = "INSERT INTO item (itemName, category, displayName, price, timestamp, isDeleted, small, medium, large, walkIn, factor)
+    VALUES ('$itemName', '$category', '$displayName', '$price', now(), 'false', '$small', '$medium', '$large', '$walkIn', '$factor')"; /*standard insert statement using the variables pulled*/
+
+    if ($conn->query($sql) === TRUE) {
+
+        echoDivWithColorSuccess('<form action="../ap_io1.html"><input type="submit" value="Go back" /></form>');
+
+        echoDivWithColorSuccess("Item created successfully");
+        echoDivWithColorSuccess("Category: $category" );
+        echoDivWithColorSuccess("Display Name: $displayName" );
+        echoDivWithColorSuccess("Item name: $itemName" );
+        echoDivWithColorSuccess("Price: $price" );
+        echoDivWithColorSuccess("Family allotment for size 1-2: $small" );
+        echoDivWithColorSuccess("Family allotment for size 3-4: $medium" );     
+        echoDivWithColorSuccess("Family allotment for size 5-6: $large" );
+        echoDivWithColorSuccess("Family allotment for walk ins: $walkIn" );       
+        echoDivWithColorSuccess("Factor: $factor" );
+
+       
+    } else {
+        echoDivWithColorFail('<form action="../ap_io1.html"><input type="submit" value="Go back" /></form>');
+        echoDivWithColorFail("Error, failed to connect to database.");
+     
+        
+    }
+
+    $conn->close();
+}
+
+?>
