7078e55345f2def39b53e8efab5dbcb49343c78a
D-Lux <Daniel_Luxa@hotmail.com>
2017-05-11 16:05:13 -0500

    Cleaned login up a little bit
    Added an error message, as well.

--- php/login.php
+++ php/login.php
@@ -1,12 +1,23 @@
 <?php
+
+	$loginErr = "";
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		$name = $_POST['login'];
-		$pw = $_POST['password'];
+		$name = fixInput($_POST['login']);
+		$pw = fixInput($_POST['password']);
 		if ($name == "admin" and $pw == "rumc") {
 			header ("location: /RUMCPantry/mainpage.html");
 		}
 		else {
-			header ("location: /RUMCPantry/login.html");
+			$loginErr = "Incorrect name or password.";
+			header ("location: /RUMCPantry/login.html?err=1");
 		}
 	}
+	
+	
+	function fixInput($data) {
+		$data = trim($data);
+		$data = stripslashes($data);
+		$data = htmlspecialchars($data);
+	 return $data;
+}
 	?>
