2b7eaf6a41321e676623ddeaf5b4358ef0658c65
D-Lux <Daniel_Luxa@hotmail.com>
2017-05-16 09:48:41 -0500

    Cleaning up Login
    Hopefully this fixes the issue we were seeing downstairs on the family
    computer.

--- php/login.php
+++ php/login.php
@@ -1,14 +1,18 @@
-<?php
-
-	$loginErr = "";
-	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+<?php	
+	if (($_SERVER["PHP_SELF"]) != "/RUMCPantry/login.html") {
+		if ($GLOBALS['loggedin'] != true) {
+			header ("location: /RUMCPantry/mainpage.html");
+		}
+	}
+	
+	elseif ($_SERVER["REQUEST_METHOD"] == "POST") {
 		$name = fixInput($_POST['login']);
 		$pw = fixInput($_POST['password']);
 		if ($name == "admin" and $pw == "rumc") {
 			header ("location: /RUMCPantry/mainpage.html");
+			$loggedin = true;
 		}
 		else {
-			$loginErr = "Incorrect name or password.";
 			header ("location: /RUMCPantry/login.html?err=1");
 		}
 	}
