3e88f30e647a37290ced4d65b532205107ba869b
Colton <colton.carter1130@gmail.com>
2017-06-19 23:03:41 -0500

    Add donation working full

--- php/donationOps.php
+++ php/donationOps.php
@@ -2,28 +2,31 @@
 
 include 'utilities.php';
 
-if(isset($_POST['createItem'])) /*when the button is pressed on post request*/
+if(isset($_POST['createDonation'])) /*when the button is pressed on post request*/
 {
     
 
-    $category = $_POST['category'];
-    $itemName = $_POST['itemName']; /*grab the name textbox*/
-    $displayName = $_POST['displayName'];
-    $price = $_POST['price'];
+    $pickupDate = $_POST['pickupDate'];
+    $networkPartner = $_POST['networkPartner']; 
+    $agency = $_POST['agency'];
+    $donorName = $_POST['donorName'];
+    $city = $_POST['city'];
 
-    $small = $_POST['small'];
-    $medium = $_POST['medium'];
-    $large = $_POST['large'];
-    $walkIn = $_POST['walkIn'];
+    $frozenNonMeat = $_POST['frozenNonMeat'];
+    $frozenMeat = $_POST['frozenMeat'];
+    $frozenPrepared = $_POST['frozenPrepared'];
+    $refBakery = $_POST['refBakery'];
+    $refProduce = $_POST['refProduce'];
+    $refDairyAndDeli = $_POST['refDairyAndDeli'];
+    $dryShelfStable = $_POST['dryShelfStable'];
+    $dryNonFood = $_POST['dryNonFood'];
+    $dryFoodDrive = $_POST['dryFoodDrive'];
 
-    $factor = $_POST['factor'];
 
-    $servername = "127.0.0.1";
-    $username = "root";
-    $password = "";
-    $dbname = "foodpantry";
 
-    $categoryID = null;
+    
+
+    $donorID = null;
 
 
 
@@ -31,76 +34,82 @@ if(isset($_POST['createItem'])) /*when the button is pressed on post request*/
     /* previous lines set up the strings for connextion*/
 
     /* Create connection*/
-    $conn = new mysqli($servername, $username, $password, $dbname);
+    $conn = createPantryDatabaseConnection();
     /* Check connection*/
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     } 
 
 
-    //check to see if category ecists, if not create it.
-        $result = $conn->query("SELECT DISTINCT name FROM Category WHERE name = '$category'");
+    //check to see if category exists, if not create it.
+        $result = $conn->query("SELECT DISTINCT name, city FROM DonationPartner WHERE name = '$donorName' && city = '$city'");
         if($result->num_rows == 0) {
         
-         $sql = "INSERT INTO category (name, small, medium, large, walkIn)
-         VALUES ('$category', 0, 0, 0, 0)";
+         $sql = "INSERT INTO DonationPartner (name, city, state, zip, address, phoneNumber)
+         VALUES ('$donorName', '$city', '', '', '', '')";
             if ($conn->query($sql) === TRUE) {
-                echoDivWithColor( "New category created: $category", "green");
+                echoDivWithColor( "New donation partner created: $donorName in $city", "green");
                 
                 
-                $sql = "SELECT DISTINCT name, categoryID FROM Category WHERE name = '$category'";
+                $sql = "SELECT DISTINCT name, donationPartnerID, city FROM DonationPartner WHERE name = '$donorName' && city = '$city'";
                 $result = $conn->query($sql);
                 if ($result->num_rows > 0) {
                         while($row = $result->fetch_assoc()) {
-                            $categoryID = $row["categoryID"];
+                            $donorID = $row["donationPartnerID"];
                         }
-                    echoDivWithColor("Category ID: $categoryID", "green" );
+                    echoDivWithColor("Donor ID: $donorID", "green" );
                 } 
           }
           else{
             echoDivWithColor('<button onclick="goBack()">Go Back</button>', "red" );
-            echoDivWithColor("Error, failed to connect to database at category insert.", "red" );
+            echoDivWithColor("Error, failed to connect to database at donation insert $sql $conn->error", "red" );
           }
 
                   
           
 
     } else {
-        $sql = "SELECT DISTINCT name, categoryID FROM Category WHERE name = '$category'";
+        $sql = "SELECT DISTINCT name, donationPartnerID, city FROM DonationPartner WHERE name = '$donorName' && city = '$city'";
             $result = $conn->query($sql);
             if ($result->num_rows > 0) {
                 while($row = $result->fetch_assoc()) {
-                    $categoryID = $row["categoryID"];
+                    $donorID = $row["donationPartnerID"];
                 }
                 
             }
-       echoDivWithColor( "$category category already in database", "green");
-       echoDivWithColor("Category ID: $categoryID", "green" );
+       echoDivWithColor( "$donorName in $city already in database", "green");
+       echoDivWithColor("Category ID: $donorID", "green" );
     }
 
 
-    $sql = "INSERT INTO item (itemName, displayName, price, timestamp, isDeleted, small, medium, large, walkIn, factor, categoryID)
-    VALUES ('$itemName', '$displayName', '$price', now(), 'false', '$small', '$medium', '$large', '$walkIn', '$factor', '$categoryID')"; /*standard insert statement using the variables pulled*/
+    $sql = "INSERT INTO Donation (donationPartnerID, dateOfPickup, networkPartner, agency, frozenNonMeat, frozenMeat, frozenPrepared, refBakery, refProduce, refDairyAndDeli, dryShelfStable, dryNonFood, dryFoodDrive)
+       VALUES ('$donorID', '$pickupDate', '$networkPartner', '$agency', '$frozenNonMeat', '$frozenMeat', '$frozenPrepared', '$refBakery', '$refProduce', '$refDairyAndDeli', '$dryShelfStable', '$dryNonFood', '$dryFoodDrive')"; /*standard insert statement using the variables pulled*/
 
     if ($conn->query($sql) === TRUE) {
 
         echoDivWithColor( '<button onclick="goBack()">Go Back</button>', "green");
 
-        echoDivWithColor("Item created successfully", "green" );
-        echoDivWithColor("Display Name: $displayName", "green" );
-        echoDivWithColor("Item name: $itemName", "green" );
-        echoDivWithColor("Price: $price", "green" );
-        echoDivWithColor("Family allotment for size 1-2: $small", "green" );
-        echoDivWithColor("Family allotment for size 3-4: $medium", "green" );     
-        echoDivWithColor("Family allotment for size 5-6: $large", "green" );
-        echoDivWithColor("Family allotment for walk ins: $walkIn", "green" );       
-        echoDivWithColor("Factor: $factor", "green" );
+        echoDivWithColor("Donation created successfully", "green" );
+        echoDivWithColor("Date of pickup: $pickupDate", "green" );
+        echoDivWithColor("Network partner: $networkPartner", "green" );
+        echoDivWithColor("Agency: $agency", "green" );
+        echoDivWithColor("Box quantites of the following:", "black" );
+        echoDivWithColor("Frozen assorted food (Non meat): $frozenNonMeat", "green" );
+        echoDivWithColor("Frozen assorted meat and seafood: $frozenMeat", "green" );
+        echoDivWithColor("Frozen assorted prepared foods: $frozenPrepared", "green" );
+        echoDivWithColor("Refridgerated assorted bakery and pastries: $refBakery", "green" );
+        echoDivWithColor("Refridgerated assorted produce: $refProduce", "green" );
+        echoDivWithColor("Refridgerated assorted dairy and deli foods: $refDairyAndDeli", "green" );
+        echoDivWithColor("Assorted foods (Shelf-stable): $dryShelfStable", "green" );
+        echoDivWithColor("Assorted non-food products: $dryNonFood", "green" );
+        echoDivWithColor("Assorted food drive foods: $dryFoodDrive", "green" );
+
         
 
        
     } else {
         echoDivWithColor('<button onclick="goBack()">Go Back</button>', "red" );
-        echoDivWithColor("Error, failed to connect to database at item insert $sql $conn->error", "red" );
+        echoDivWithColor("Error, failed to connect to database at donation insert $sql $conn->error", "red" );
      
         
     }
