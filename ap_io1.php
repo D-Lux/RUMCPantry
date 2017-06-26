1048378e71f69817c819d5e9163f64012a1f3bab
Colton <colton.carter1130@gmail.com>
2017-06-26 14:39:55 -0500

    testing to see if this works

---
+++ ap_io1.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+
+<head>
+    <script src="js/utilities.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/toolTip.css">
+
+    <title>ap_io1</title>
+
+</head>
+
+<body>
+    <button onclick="goBack()">Go Back</button>
+    <h1>
+        Admin page inventory ops 1
+    </h1>
+    <form method="get" action="ap_io2.html">
+        <input type="submit" value="Add an item">
+    </form>
+    <form method="get" action="ap_io3.html">
+        <input type="submit" value="Edit an item">
+    </form>
+    <form method="get" action="aof.html">
+        <input type="submit" value="Edit order form">
+    </form>
+    <form method="get" action="ap_io4.html">
+        <input type="submit" value="Add a category">
+    </form>
+
+    <?php include 'php/displayItems.php';?>
+    <?php include 'php/displayCategories.php';?>
+</body>
+
+</html>
