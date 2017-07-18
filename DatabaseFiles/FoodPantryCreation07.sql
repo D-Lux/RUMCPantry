670563c450b89ae3b004a29ac49adb086f128781
Colton <colton.carter1130@gmail.com>
2017-07-18 12:00:03 -0500

    sql was screwed up. fixed it

--- DatabaseFiles/FoodPantryCreation07.sql
+++ DatabaseFiles/FoodPantryCreation07.sql
@@ -1,7 +1,5 @@
 -- MySQL Workbench Forward Engineering
-drop database foodpantry;
--- MySQL Workbench Forward Engineering
-
+drop table if exists foodpantry;
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
