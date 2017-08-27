63dea53e92a1c692c155d5280e54a9d5e9c9d505
D-Lux <Daniel_Luxa@hotmail.com>
2017-08-27 17:48:27 -0500

    CSS v3
    Christmas time.

--- cp1.php
+++ cp1.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-
-<html>
-	<head>
-		<title>Roselle United Methodist Church Food Pantry</title>
-		<script src="js/utilities.js"></script>
-		<link href='style.css' rel='stylesheet'>
-		<?php include 'php/utilities.php'; ?>
-
-	</head>
-	<body>
+<?php include 'php/utilities.php'; ?>
 		<script>
 		// This function updates a hidden input field with the appropriate clientID
 		function updateHiddenClientID() {
@@ -35,60 +25,59 @@
 			removeCookie("clientApptSet");
 		}
 		</script>
-		<button onclick="goBack()">Back</button>
+		<button id='btn_back' onclick="goBack()">Back</button>
 		
-		<h1>Roselle United Methodist Church</h1>
-		<h2>Food Pantry</h2>
 		<h3>Client Selection</h3>
-	
-		<?php
-			$conn = createPantryDatabaseConnection();
-			if ($conn->connect_error) {
-				die("Connection failed: " . $conn->connect_error);
-			}
-			
-			// *******************************************
-			// ** Generate the datalist for client drop down
-			// ** Restrict dropdown to people with appointments today
-			
-			$sql = "SELECT firstName AS fName, lastName AS lName, FamilyMember.clientID as clientID
-					FROM FamilyMember
-					JOIN Client
-					ON FamilyMember.clientID = Client.clientID
-					WHERE isHeadOfHousehold=1
-					AND Client.isDeleted=0
-					AND Client.redistribution=0
-					AND (firstName <> 'Available'
-					AND lastName <> 'Available')";
-			$clientInfo = queryDB($conn, $sql);
+		<div class='body_content'>
+			<?php
+				$conn = createPantryDatabaseConnection();
+				if ($conn->connect_error) {
+					die("Connection failed: " . $conn->connect_error);
+				}
+				
+				// *******************************************
+				// ** Generate the datalist for client drop down
+				// ** Restrict dropdown to people with appointments today
+				
+				$sql = "SELECT firstName AS fName, lastName AS lName, FamilyMember.clientID as clientID
+						FROM FamilyMember
+						JOIN Client
+						ON FamilyMember.clientID = Client.clientID
+						WHERE isHeadOfHousehold=1
+						AND Client.isDeleted=0
+						AND Client.redistribution=0
+						AND (firstName <> 'Available'
+						AND lastName <> 'Available')";
+				$clientInfo = queryDB($conn, $sql);
 
-			if (($clientInfo == NULL) || ($clientInfo->num_rows <= 0)) {
-				echo "No clients in the database.";
-			}
-			
-			// Generate the string we'll need to display the client datalist
-			$clientDataList = "<input type='text' name='clientName' list='Clients' autocomplete='off' id='clientID'";
-			$clientDataList .= " onchange='updateHiddenClientID()'><datalist id='Clients' >";
-			while($client = sqlFetch($clientInfo) ) {
-				$clientDataList .= "<option data-value=" . $client['clientID'] . ">";
-				$clientDataList .= $client['lName'] . ", " . $client['fName'] . "</option>";
+				if (($clientInfo == NULL) || ($clientInfo->num_rows <= 0)) {
+					echo "No clients in the database.";
+				}
+				
+				// Generate the string we'll need to display the client datalist
+				$clientDataList = "<input type='text' name='clientName' list='Clients' autocomplete='off' id='clientID'";
+				$clientDataList .= " onchange='updateHiddenClientID()'><datalist id='Clients' >";
+				while($client = sqlFetch($clientInfo) ) {
+					$clientDataList .= "<option data-value=" . $client['clientID'] . ">";
+					$clientDataList .= $client['lName'] . ", " . $client['fName'] . "</option>";
 
-			}
-			$clientDataList .= "</datalist>";
-			
-			// Close the connection
-			closeDB($conn);
-		
-
-			echo "<form action='cp2.php' method='post' >";
-			echo "Name: ";
-			echo $clientDataList . "<br><br>";
-			echo "<input type='hidden' ID='clientID-hidden' name='clientID' value=0>";
-			echo "<input type='submit' value='Continue'>";
-			echo "</form>";
+				}
+				$clientDataList .= "</datalist>";
+				
+				// Close the connection
+				closeDB($conn);
 			
 
-		?>		
+				echo "<form action='cp2.php' method='post' >";
+				echo "Name: ";
+				echo $clientDataList . "<br><br>";
+				echo "<input type='hidden' ID='clientID-hidden' name='clientID' value=0>";
+				echo "<input type='submit' value='Continue'>";
+				echo "</form>";
+				
+
+			?>
+		</div><!-- closing .body_content -->
 
 	</body>
 </html>
